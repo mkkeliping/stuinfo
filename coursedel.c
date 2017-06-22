a4ef42cb7a75f4d1782963a02cfd70ba4d2d735f
mk <klp666666@outlook.com>
2017-06-22 02:58:52 -0700

    coursse manager
    course manager

---
+++ coursedel.c
@@ -0,0 +1,58 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <mysql/mysql.h>
+#include "cgic.h"
+
+int cgiMain()
+{
+
+	fprintf(cgiOut, "Content-type:text/html;charset=utf-8\n\n");
+
+	char Cno[32] = "\0";
+
+	int status = 0;
+
+
+	status = cgiFormString("Cno",  Cno, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get Cno error!\n");
+		return 1;
+	}
+
+	int ret;
+	char sql[128] = "\0";
+	MYSQL *db;
+
+	//初始化
+	db = mysql_init(NULL);
+	if (db == NULL)
+	{
+		fprintf(cgiOut,"mysql_init fail:%s\n", mysql_error(db));
+		return -1;
+	}
+
+	//连接数据库
+	db = mysql_real_connect(db, "127.0.0.1", "root", "123456", "STU",  3306, NULL, 0);
+	if (db == NULL)
+	{
+		fprintf(cgiOut,"mysql_real_connect fail:%s\n", mysql_error(db));
+		mysql_close(db);
+		return -1;
+	}
+mysql_options(db,MYSQL_SET_CHARSET_NAME,"utf8");
+
+	sprintf(sql, "delete from Course where Cno = '%s'", Cno);
+	if ((ret = mysql_real_query(db, sql, strlen(sql) + 1)) != 0)
+	{
+		fprintf(cgiOut,"mysql_real_query fail:%s\n", mysql_error(db));
+		mysql_close(db);
+		return ;
+	}
+	fprintf(cgiOut, "delete Course ok!\n");
+	mysql_close(db);
+
+	return 0;
+}
